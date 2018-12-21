ac7e5dc575472b6166876bb17b4b7a17afd1e387
stepan <abc@abc.com>
2018-12-20 23:43:06 -0800

    adding more rss feed site

--- web_server/views/main.js
+++ web_server/views/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueNativeSock from 'vue-native-websocket'
-// Vue.use(VueNativeSock, 'ws://18.191.245.20:3001/feeds', {
-Vue.use(VueNativeSock, 'ws://localhost:3001/feeds', {
+Vue.use(VueNativeSock, 'ws://18.191.245.20:3001/feeds', {
+// Vue.use(VueNativeSock, 'ws://localhost:3001/feeds', {
 	reconnection: true,
 });
 
