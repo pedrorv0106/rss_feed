a18a9bf34030bb44ce9aff42e7014d0185e4e2cc
stepan <abc@abc.com>
2018-12-27 22:40:29 -0800

    updating css working

--- web_server/views/global.js
+++ web_server/views/global.js
@@ -1,8 +1,8 @@
-// const API_URL = 'http://18.191.245.20:3000/news/feed';
-const API_URL = 'http://localhost:3000/news/feed';
+const API_URL = 'http://18.191.245.20:3000/news/feed';
+// const API_URL = 'http://localhost:3000/news/feed';
 
-// const WS_URL = 'ws://18.191.245.20:3001/feeds';
-const WS_URL = 'ws://localhost:3001/feeds';
+const WS_URL = 'ws://18.191.245.20:3001/feeds';
+// const WS_URL = 'ws://localhost:3001/feeds';
 
 exports.API_URL = API_URL
 exports.WS_URL = WS_URL
